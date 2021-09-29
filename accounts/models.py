9e3310cd24565045e96b1419d19ea091e5859509
Promise Dada <gdadapromise@gmail.com>
2021-09-29 12:44:24 -0400

    password reset

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,6 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from phone_field import PhoneField
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="customer")
     address = models.CharField(max_length=250)
@@ -22,14 +21,11 @@ class Profile(models.Model):
             Profile.objects.get_or_create(user=instance)
             print("Customer profile created successfully")
 
-    # @receiver(post_save, sender=User)
-    # def update_customer_profile(sender, instance, created, *args, **kwargs):
-    #     if created != True:
-    #         # print(dir(instance))
-    #         # instance.save()
-    #         instance.profile.save()
-    #         print("Customer profile details for updated successfully")
+    @receiver(post_save, sender=User)
+    def update_customer_profile(sender, instance, created, *args, **kwargs):
+        if created != True:
+            instance.profile.save()
+            print("Customer profile details for updated successfully")
 
     def __str__(self):
         return self.user.username
-
